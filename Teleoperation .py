b5e623afee19e8ea3a730207eb5a7217f2f0b245
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 12:19:51 +0530

    Create Teleoperation .py

---
+++ Teleoperation .py
@@ -0,0 +1,12 @@
+# SESSION 34 – Teleoperation (Keyboard Control)
+
+# Step 1: Input key
+key = 'A'  # simulated key press
+
+# Step 2: Map key to action
+if key == 'A':
+    print("Left Turn")
+else:
+    print("No Action")
+
+print("\nProgram Executed Successfully")
